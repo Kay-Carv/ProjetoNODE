056417671077f5ade3ea4019e938ac1015b88d6d
Kayque <kayquecarvalho020@gmail.com>
2025-02-24 12:19:24 -0300

    Adicionando anotações de comando e executar o servidor na porta 3000

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express")
 
 const app =express();
 
+// criando um endpoint e requisições
 app.get("/login", (req, res)=>{
     res.send("Bem vindo ao login")
 });
 
 
-app.listen(3000, ()=>{
-    console.log("Servidor rodadando na porta 3000")
+// executando o servidor na porta 3000
+app.listen(3000,()=>{
+    console.log('Servidor rodando na porta 3000')
+})
+
+app.get('/sistema',(req,res)=>{
+    res.send('Acessando o sistema')
 })
